8eb95f41384dc8d39c2cd2513444e1a105047115
Reche Soares <rechesoares13@gmail.com>
2025-05-13 10:08:06 +0530

    create docs layout

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -2,10 +2,12 @@ import { ReactNode } from "react";
 
 export default function DocsLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="w-full flex-col">
-      <header className="h-20">header</header>
-      <div className="px-6 lg:px-8">
-        <main>{children}</main>
+    <div className="w-full flex-col bg-neutral-950">
+      <header className="h-16 text-white">header</header>
+      <div className="mx-auto max-w-[1536px] text-white flex space-x- h-full">
+        <div className="w-[300px] h-[calc(100dvh-64px)]">navigation</div>
+        <main className="flex-1">{children}</main>
+        <div className="w-[300px] h-full border">aside</div>
       </div>
     </div>
   );
