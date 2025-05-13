2404d25cb80478337b1796eb3eb2c4128ac05464
Reche Soares <rechesoares13@gmail.com>
2025-05-13 17:05:05 +0530

    add sidebar navigation

--- src/components/site/header.tsx
+++ src/components/site/header.tsx
@@ -3,7 +3,7 @@ import { ThemeSwitcher } from "./theme";
 
 const Header = () => {
   return (
-    <div className="w-full sticky top-0 z-20 bg-white dark:bg-zinc-950 border-b border-gray-300 dark:border-zinc-800">
+    <div className="w-full sticky top-0 z-20 bg-white dark:bg-zinc-950 border-b border-gray-200 dark:border-zinc-800">
       <header className="mx-auto max-w-[1536px] h-16">
         header
         <ThemeSwitcher />
