04f535db70464b20b23fabe7ea6184975c982f74
Reche Soares <rechesoares13@gmail.com>
2025-05-13 19:13:41 +0530

    add documentation styling

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -7,9 +7,11 @@ export default function DocsLayout({ children }: { children: ReactNode }) {
   return (
     <div className="w-full flex-col bg-white dark:bg-neutral-950 text-neutral-800 dark:text-white relative">
       <Header />
-      <div className="mx-auto max-w-[1536px] text-white flex flex-1">
+      <div className="mx-auto max-w-[1536px] text-white flex flex-1 space-x-10">
         <Sidebar />
-        <main className="flex-1 py-6">{children}</main>
+        <main className="flex-1 py-6 prose prose-zinc pb-16 pt-8 dark:prose-invert prose-h1:text-3xl prose-h1:font-semibold prose-h2:text-xl prose-h2:font-semibold prose-h3:text-lg prose-h3:font-medium max-w-full">
+          {children}
+        </main>
         <aside className="w-[300px] h-[calc(100dvh-57px)] sticky top-[57px]">
           <ScrollArea className="w-full h-full">aside</ScrollArea>
         </aside>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { ClassNameValue, twMerge } from "tailwind-merge";
 export const cn = (...classNames: ClassNameValue[]) => {
   return twMerge(clsx(...classNames));
 };
+
+export const createSlug = (text: string) => {
+  return text
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]+/g, "");
+};
