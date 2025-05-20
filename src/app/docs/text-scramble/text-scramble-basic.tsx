82272b15b79992ac4b178b71c09f17aa07fd3598
Reche Soares <rechesoares13@gmail.com>
2025-05-20 11:15:38 +0530

    update text scramble docs

--- src/app/docs/text-scramble/text-scramble-basic.tsx
+++ src/app/docs/text-scramble/text-scramble-basic.tsx
@@ -2,7 +2,9 @@ import { TextScramble } from "@/components/core/text-scramble";
 
 const TextScrambleBasic = () => {
   return (
-    <TextScramble className="font-mono uppercase">Its over 9000!</TextScramble>
+    <TextScramble className="font-mono text-2xl uppercase">
+      Its over 9000!
+    </TextScramble>
   );
 };
 
