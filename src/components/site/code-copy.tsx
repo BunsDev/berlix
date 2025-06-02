a4f6dd4fdc0d95621d92a7cae95285dc8daaf10a
Reche Soares <rechesoares13@gmail.com>
2025-06-02 16:19:30 +0530

    update cli-command-copy ui with multiple pm choices

--- src/components/site/code-copy.tsx
+++ src/components/site/code-copy.tsx
@@ -1,8 +1,15 @@
 "use client";
+import { cn } from "@/lib/utils";
 import { Check, Clipboard } from "lucide-react";
 import React, { useState } from "react";
 
-const CodeCopy = ({ code }: { code: string }) => {
+const CodeCopy = ({
+  code,
+  className,
+}: {
+  code: string;
+  className?: string;
+}) => {
   const [copied, setCopied] = useState(false);
 
   const copyCode = () => {
@@ -17,21 +24,24 @@ const CodeCopy = ({ code }: { code: string }) => {
   return (
     <button
       onClick={copyCode}
-      className="absolute top-4 right-5 cursor-pointer z-20 p-2"
+      className={cn(
+        "absolute top-4 right-5 cursor-pointer z-20 p-2 text-zinc-50",
+        className
+      )}
     >
       <div
         className={`absolute inset-0 transform transition-all duration-300 ${
           copied ? "scale-0 opacity-0" : "scale-100 opacity-100"
         }`}
       >
-        <Clipboard className="h-4 w-4 text-zinc-50" />
+        <Clipboard className="h-4 w-4" />
       </div>
       <div
         className={`absolute inset-0 transform transition-all duration-300 ${
           copied ? "scale-100 opacity-100" : "scale-0 opacity-0"
         }`}
       >
-        <Check className="h-4 w-4 text-zinc-50" />
+        <Check className="h-4 w-" />
       </div>
     </button>
   );
