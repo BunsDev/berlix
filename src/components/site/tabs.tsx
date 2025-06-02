33dd24a41e0c249a40b41339c9ece0a8cc5dce3a
Reche Soares <rechesoares13@gmail.com>
2025-06-02 13:21:40 +0530

    upadte tabs with motion

--- src/components/site/tabs.tsx
+++ src/components/site/tabs.tsx
@@ -4,8 +4,24 @@ import * as React from "react";
 import * as TabsPrimitive from "@radix-ui/react-tabs";
 
 import { cn } from "@/lib/utils";
+import { motion } from "motion/react";
 
 const Tabs = TabsPrimitive.Root;
+
+const TabsContext = React.createContext<string>("");
+
+const TabsRoot = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Root>
+>(({ ...props }, ref) => {
+  const uniqueId = React.useId();
+  return (
+    <TabsContext.Provider value={uniqueId}>
+      <Tabs ref={ref} {...props} />
+    </TabsContext.Provider>
+  );
+});
+TabsRoot.displayName = TabsPrimitive.Root.displayName;
 
 const TabsList = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.List>,
@@ -14,7 +30,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-10 w-full items-center justify-start border-b-[1.5px] border-zinc-300 dark:border-zinc-700",
+      "inline-flex h-10 w-full items-center justify-start border-b border-zinc-200 bg-transparent text-zinc-900 dark:border-zinc-800 dark:text-zinc-50",
       className
     )}
     {...props}
@@ -24,17 +40,61 @@ TabsList.displayName = TabsPrimitive.List.displayName;
 
 const TabsTrigger = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.Trigger
-    ref={ref}
-    className={cn(
-      "inline-flex items-center cursor-pointer justify-center whitespace-nowrap px-3 py-1 text-sm text-zinc-600 dark:text-zinc-400 font-semibold ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:text-zinc-900 dark:data-[state=active]:text-zinc-100",
-      className
-    )}
-    {...props}
-  />
-));
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger> & {
+    classNameIndicator?: string;
+  }
+>(({ className, children, classNameIndicator, ...props }) => {
+  const triggerRef = React.useRef<HTMLButtonElement>(null);
+  const [isActive, setIsActive] = React.useState(false);
+  const tabsId = React.useContext(TabsContext);
+
+  React.useEffect(() => {
+    const element = triggerRef.current;
+    if (element) {
+      setIsActive(element.dataset.state === "active");
+
+      const observer = new MutationObserver(() => {
+        setIsActive(element.dataset.state === "active");
+      });
+
+      observer.observe(element, { attributes: true });
+
+      return () => observer.disconnect();
+    }
+  }, []);
+
+  return (
+    <TabsPrimitive.Trigger
+      ref={triggerRef}
+      className={cn(
+        "relative inline-flex h-10 items-center justify-center rounded-none bg-transparent px-4 py-1 pt-2 pb-3 text-sm font-medium whitespace-nowrap text-zinc-500 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:text-zinc-900 dark:text-zinc-400 dark:data-[state=active]:text-zinc-50",
+        className
+      )}
+      {...props}
+    >
+      {isActive && (
+        <motion.div
+          className="absolute bottom-0 flex h-0.5 w-full justify-center"
+          transition={{
+            type: "spring",
+            stiffness: 26.7,
+            damping: 4.1,
+            mass: 0.2,
+          }}
+          layoutId={`underline-${tabsId}`}
+        >
+          <div
+            className={cn(
+              "h-0.5 w-4/5 bg-zinc-950 dark:bg-zinc-50",
+              classNameIndicator
+            )}
+          />
+        </motion.div>
+      )}
+      {children}
+    </TabsPrimitive.Trigger>
+  );
+});
 TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
 
 const TabsContent = React.forwardRef<
@@ -44,7 +104,7 @@ const TabsContent = React.forwardRef<
   <TabsPrimitive.Content
     ref={ref}
     className={cn(
-      "mt-4 mb-10 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 border border-zinc-200 dark:border-zinc-800 rounded-lg",
+      "focus-visible:ring-ring relative mt-2 rounded-lg ring-offset-blue-50 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-hidden",
       className
     )}
     {...props}
@@ -52,4 +112,4 @@ const TabsContent = React.forwardRef<
 ));
 TabsContent.displayName = TabsPrimitive.Content.displayName;
 
-export { Tabs, TabsList, TabsTrigger, TabsContent };
+export { TabsRoot as Tabs, TabsList, TabsTrigger, TabsContent };
