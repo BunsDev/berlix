dfd384d592d3e77480743c3205fd8b2d32b6a125
Reche Soares <rechesoares13@gmail.com>
2025-06-02 10:34:47 +0530

    add motion to hero buttons

--- src/app/(landing)/_components/hero.tsx
+++ src/app/(landing)/_components/hero.tsx
@@ -1,20 +1,23 @@
+"use client";
+
+import { motion } from "motion/react";
 import { GitHubIcon } from "@/assets/icons/github";
 import { ArrowRight } from "lucide-react";
 
 export const Hero = () => {
   return (
-    <div className="mt-20 border flex justify-between gap-20">
+    <div className="mt-20 flex justify-between gap-20">
       <div className="flex-1">
         <h1 className="font-bold flex flex-col text-5xl text-zinc-900 dark:text-zinc-50 capitalize tracking-tight leading-[1.3]">
           <span className="inline-block">
             Breathe{" "}
-            <span className="bg-gradient-to-b from-pink-400 to-rose-600 bg-clip-text text-transparent">
+            <span className="bg-gradient-to-r from-orange-500 to-rose-600 bg-clip-text text-transparent">
               Motion
             </span>
           </span>
           <span className="inline-block">
             into your{" "}
-            <span className="bg-gradient-to-b from-purple-400 to-violet-600 bg-clip-text text-transparent">
+            <span className="bg-gradient-to-r from-fuchsia-500 via-violet-600 to-blue-600 bg-clip-text text-transparent">
               Components
             </span>
           </span>
@@ -23,17 +26,22 @@ export const Hero = () => {
           Motion-first components for React, built with Tailwind CSS and Motion.
         </p>
         <div className="mt-12 flex items-center gap-4">
-          <button className="cursor-pointer flex items-center gap-2 px-8 h-12 py-3 rounded-full bg-zinc-950 text-zinc-50 font-medium dark:bg-zinc-50 dark:text-zinc-900">
+          <motion.button
+            whileHover="hover"
+            className="cursor-pointer flex items-center gap-2 px-8 h-12 py-3 rounded-full bg-zinc-950 text-zinc-50 font-medium dark:bg-zinc-50 dark:text-zinc-900"
+          >
             <span>Get Started</span>
-            <ArrowRight />
-          </button>
+            <motion.div variants={{ hover: { x: 5 } }}>
+              <ArrowRight />
+            </motion.div>
+          </motion.button>
           <button className="cursor-pointer flex items-center gap-2 h-12 px-8 py-3 rounded-full border-2 border-zinc-950 dark:text-zinc-50 font-medium dark:border-zinc-50 text-zinc-900">
             <GitHubIcon className="size-4 fill-zinc-950 dark:fill-zinc-50" />
             Star on GitHub
           </button>
         </div>
       </div>
-      <div className="flex-1 border">{/* TODO: image */}</div>
+      <div className="flex-1">{/* TODO: image */}</div>
     </div>
   );
 };
